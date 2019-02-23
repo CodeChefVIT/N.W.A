c903dbcc5740e205b9448a00a8d9f5a00cc95413
tushar1210 <ts26192271@gmail.com>
2019-02-23 05:31:54 +0530

    Tech Review 1

---
+++ Crimson/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Crimson
+//
+//  Created by Tushar Singh on 23/02/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var letsGo: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        letsGo.layer.cornerRadius = 15
+    }
+
+
+}
+
